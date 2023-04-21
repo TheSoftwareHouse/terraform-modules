d9b347797abe542f6de99ae2da50a8dc9151ac45
Damian Tykalowski <damian@d47zm3.me>
2023-04-21 15:18:48 +0200

    Add AWS Accounts Basics Module

--- aws_account_basics/modules/aws_cloudformation_stack/providers.tf
+++ aws_account_basics/modules/aws_cloudformation_stack/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      configuration_aliases = [ aws.regional ]
+      source                = "hashicorp/aws"
+      configuration_aliases = [aws]
     }
   }
 }
