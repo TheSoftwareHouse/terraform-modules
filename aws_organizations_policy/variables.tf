88374f57bac055a9856819f02eba4963fb60a9d6
Michal Rybacki <michalrybacki@Michals-MacBook-Pro.local>
2023-11-30 20:55:30 +0100

    DEVOPS-1462-scp - fix

--- aws_organizations_policy/variables.tf
+++ aws_organizations_policy/variables.tf
@@ -9,12 +9,4 @@ variable "policies" {
       policy_target_id  = string
     }
   ))
-  default = {
-    name             = ""
-    content          = ""
-    description      = "Service Control Policy attached to organization."
-    type             = "SERVICE_CONTROL_POLICY"
-    tags             = {}
-    policy_target_id = ""
-  }
 }
