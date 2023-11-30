3aa940f86a32ae98332215aabeed7e1085a6c88d
Michal Rybacki <michalrybacki@Michals-MacBook-Pro.local>
2023-11-30 21:02:19 +0100

    DEVOPS-1462-scp - corrected outputs

--- aws_organizations_policy/outputs.tf
+++ aws_organizations_policy/outputs.tf
@@ -1,11 +1,3 @@
-output "id" {
-  value = aws_organizations_policy.this.arn
-}
-
-output "arn" {
-  value = aws_organizations_policy.this.arn
-}
-
-output "tags_all" {
-  value = aws_organizations_policy.this.arn
+output "policies_arns" {
+  value = { for k, acc in aws_organizations_policy.this : k => acc.arn }
 }
